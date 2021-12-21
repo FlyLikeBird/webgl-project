d4bfd4faf91f1e1ff1bec92ae6e7cc5979bdaebf
FlyLikeBird <renshanhang@163.com>
2021-12-21 18:00:32 +0800

    add Chapter5

--- Chapter5/5-4/MultiAttributeColor.js
+++ Chapter5/5-4/MultiAttributeColor.js
@@ -2,9 +2,11 @@
 // Vertex shader program
 var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
+  'attribute float a_PointSize;\n' +
   'uniform mat4 u_ModelMatrix;\n' +
   'void main() {\n' +
-  '  gl_Position = u_ModelMatrix * a_Position;\n' +
+  '  gl_Position =  a_Position;\n' +
+  '  gl_PointSize = 10;\n' + 
   '}\n';
 
 // Fragment shader program
@@ -18,32 +20,10 @@ function main(){
     var gl = canvas.getContext('webgl');
     initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-    var matrix = new Matrix4();
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
     var n = initVertexBuffer();
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    var i = 0;
-    var last_time = Date.now();
-    var angle_step = 15.0;
-    var currentAngle = 0.0;
-    function render(){
-        gl.clear(gl.COLOR_BUFFER_BIT); 
-        currentAngle = getAngle(currentAngle);   
-        matrix.setRotate(currentAngle, 0, 0, 1);
-        matrix.translate(0.3, 0, 0);
-        // matrix.rotate(currentAngle, 0, 1, 0 );
-        gl.uniformMatrix4fv(u_ModelMatrix, false, matrix.elements);
-        gl.drawArrays(gl.TRIANGLES, 0, n);
-        requestAnimationFrame(render);
-    }
-    render();
-    function getAngle(angle){
-        let now = Date.now();
-        var timeDis = now - last_time;
-        last_time = now;
-        let result = angle + angle_step * timeDis / 1000;
-        return result %= 360;
-    }
+    gl.drawArrays(gl.POINTS, 0, n);
     function initVertexBuffer(){
         // var vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.8, -0.3, 0.6, -0.9]);
         // 以顶点为起始点，逆时针方向绘制三角形来判断最后一条边;
@@ -51,12 +31,21 @@ function main(){
         var vertices = new Float32Array([
             0.0, 0.3, -0.3, -0.3, 0.3, -0.3
         ]);
+        var sizes = new Float32Array([
+            10.0, 20.0, 30.0
+        ]);
         var n = 3;
         var vertexBuffer = gl.createBuffer();
+        var sizeBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
         gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(a_Position);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_PointSize);
         return n;
     }
    
